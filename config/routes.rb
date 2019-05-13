9756c4fa7b1385dfbaeb21fd4617d7d9987ad25f
Renato Rosa Franco <Renato_ny@live.com>
2019-05-12 23:42:18 -0300

    Project Updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,21 @@
 # frozen_string_literal: true
 Rails.application.routes.draw do
 
+  # Authentication
+  # @implemented
+  # devise_for :users
+
+  # Application
+  # @implemented
   root to: 'home#index'
+  get '/faq',          to: 'home#faq'
+  get '/about',        to: 'home#about'
+  get '/contributing', to: 'home#contributing'
+  get '/terms-of-use', to: 'home#terms-of-use'
+
+  # Blog
+  # @implemented
+  namespace :blog, constraint: {subdomain: 'blog'} do
+    resources :posts, only: [:index, :show]
+  end
 end
