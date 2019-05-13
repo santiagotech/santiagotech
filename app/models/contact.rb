07ad7361e2bfa443c79f41111dcc43d45879300b
Renato Rosa Franco <Renato_ny@live.com>
2019-05-13 20:35:11 -0300

    Contact validations added

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -16,4 +16,34 @@ class Contact
   def persisted?
     false
   end
+
+  validates :name,
+            presence: true,
+            uniqueness: false,
+            allow_blank: false,
+            length: { minimum: 3, maximum: 30 }
+
+  validates :phone,
+            presence: true,
+            uniqueness: false,
+            allow_blank: false,
+            length: { minimum: 3, maximum: 30 }
+  
+  validates :email,
+            presence: true,
+            uniqueness: false,
+            allow_blank: false,
+            length: { minimum: 3, maximum: 245 }
+  
+  validates :subject,
+            presence: true,
+            uniqueness: false,
+            allow_blank: false,
+            length: { minimum: 3, maximum 140 }
+  
+  validates :message,
+            presence: true,
+            uniqueness: false,
+            allow_blank: false,
+            length: { minimum: 3, maximum: 4_000 }
 end
