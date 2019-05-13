db0ac8b066060bae2aa46ec33a74f1d07ecc9e6d
Renato Rosa Franco <Renato_ny@live.com>
2019-05-13 01:13:27 -0300

    Updating Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,24 +9,28 @@
 # State
 # @implemented
 State.destroy_all
-State.create([
-  {
-    name: 'Rio Grande do Sul',
-    region: 'Sul',
-    acronym: 'RS'
-  }
-])
+State.create(
+  [
+    {
+      name: 'Rio Grande do Sul',
+      region: 'Sul',
+      acronym: 'RS'
+    }
+  ]
+)
 
 # City
 # @implemented
 state = State.where(name: 'Rio Grande do Sul').first()
 City.destroy_all
-City.create([
-  {
-    name: 'Santiago', 
-    state: state
-  }
-])
+City.create(
+  [
+    {
+      name: 'Santiago', 
+      state: state
+    }
+  ]
+)
 
 # Neighborhood
 # @implemented
@@ -72,71 +76,230 @@ Neighborhood.create(
       name: 'B Belizario',
       city: city
     },
-    {name: 'Bairro Gaspar Dutra', city: city},
-    {name: 'Bairro Vila Leste', city: city},
-    {name: 'Bairro Zampereti', city: city},
-    {name: 'Belizario', city: city},
-    {name: 'Belizario de Souza', city: city},
+    {
+      name: 'Bairro Gaspar Dutra', 
+      city: city
+    },
+    {
+      name: 'Bairro Vila Leste', 
+      city: city
+    },
+    {
+      name: 'Bairro Zampereti',
+      city: city
+    },
+    {
+      name: 'Belizario', 
+      city: city
+    },
+    {
+      name: 'Belizario de Souza', 
+      city: city
+    },
     # C
-    {name: 'Carlos Humberto', city: city},
-    {name: 'Castilhos', city: city},
-    {name: 'Centro', city: city},
-    {name: 'Ceu Aberto', city: city},
-    {name: 'Condomínio jardim ipe', city: city},
-    {name: 'Corrd Tropas', city: city}
+    {
+      name: 'Carlos Humberto', 
+      city: city
+    },
+    {
+      name: 'Castilhos', 
+      city: city
+    },
+    {
+      name: 'Centro', 
+      city: city
+    },
+    {
+      name: 'Ceu Aberto', 
+      city: city
+    },
+    {
+      name: 'Condomínio jardim ipe', 
+      city: city
+    },
+    {
+      name: 'Corrd Tropas', 
+      city: city
+    },
     # D
-    {name: 'Daer', city: city},
-    {name: 'Distrito de Santiag', city: city},
-    {name: 'Distrito Industrial', city: city}
+    {
+      name: 'Daer', 
+      city: city
+    },
+    {
+      name: 'Distrito de Santiag', 
+      city: city
+    },
+    {
+      name: 'Distrito Industrial', 
+      city: city
+    }
     # G
-    {name: 'Gaspar Dutra', city: city},
-    {name: 'Guabiróba', city: city},
+    {
+      name: 'Gaspar Dutra', 
+      city: city
+    },
+    {
+      name: 'Guabiróba', 
+      city: city
+    },
     # I
-    {name: 'Interior', city: city},
-    {name: 'Irmã Dulce', city: city},
-    {name: 'Itu', city: city},
+    {
+      name: 'Interior', 
+      city: city
+    },
+    {
+      name: 'Irmã Dulce', 
+      city: city
+    },
+    {
+      name: 'Itu', 
+      city: city
+    },
     # J
-    {name: 'Jardim América', city: city},
-    {name: 'Jardim Botânico', city: city},
-    {name: 'Jardim Ipê', city: city},
-    {name: 'Jardim Eucaliptos', city: city},
-    {name: 'Jardim Eucalipt', city: city},
-    {name: 'João Evangelista', city: city},
+    {
+      name: 'Jardim América', 
+      city: city
+    },
+    {
+      name: 'Jardim Botânico', 
+      city: city
+    },
+    {
+      name: 'Jardim Ipê', 
+      city: city
+    },
+    {
+      name: 'Jardim Eucaliptos', 
+      city: city
+    },
+    {
+      name: 'Jardim Eucalipt', 
+      city: city
+    },
+    {
+      name: 'João Evangelista', 
+      city: city
+    },
     # L
-    {name: 'Loteamento Darci Schiffler', city: city},
-    {name: 'Lulu Genro', city: city},
-    {name: 'Lulu Santos', city: city},
+    {
+      name: 'Loteamento Darci Schiffler', 
+      city: city
+    },
+    {
+      name: 'Lulu Genro', 
+      city: city
+    },
+    {
+      name: 'Lulu Santos', 
+      city: city
+    },
     # M
-    {name: 'Maria Alice Gomes', city: city},
-    {name: 'Missões', city: city},
-    {name: 'Monsenhor Assis', city: city},
-    {name: 'Monte Alegre', city: city},
+    {
+      name: 'Maria Alice Gomes', 
+      city: city
+    },
+    {
+      name: 'Missões', 
+      city: city
+    },
+    {
+      name: 'Monsenhor Assis', 
+      city: city
+    },
+    {
+      name: 'Monte Alegre', 
+      city: city
+    },
     # N
-    {name: 'Nei Pereira', city: city},
+    {
+      name: 'Nei Pereira', 
+      city: city
+    },
     # P
-    {name: 'Padre Assis', city: city},
-    {name: 'Presidente João Goulart', city: city},
+    {
+      name: 'Padre Assis', 
+      city: city
+    },
+    {
+      name: 'Presidente João Goulart', 
+      city: city
+    },
     # R
-    {name: 'Riachuelo', city: city},
-    {name: 'Rincão Castilhos', city: city},
+    {
+      name: 'Riachuelo', 
+      city: city
+    },
+    {
+      name: 'Rincão Castilhos', 
+      city: city
+    },
     # S
-    {name: 'Santiago Pompeu', city: city},
-    {name: 'São Jorge', city: city},
-    {name: 'São Vicente', city: city},
-    {name: 'Sede', city: city},
-    {name: 'Suburbios', city: city},
+    {
+      name: 'Santiago Pompeu', 
+      city: city
+    },
+    {
+      name: 'São Jorge', 
+      city: city
+    },
+    {
+      name: 'São Vicente', 
+      city: city
+    },
+    {
+      name: 'Sede', 
+      city: city
+    },
+    {
+      name: 'Suburbios', 
+      city: city
+    },
     # V
-    {name: 'Vila Belizario', city: city},
-    {name: 'Vila Carlos Humbero', city: city},
-    {name: 'Vila das Paineiras', city: city},
-    {name: 'Vila Itu', city: city},
-    {name: 'Vila Jardim', city: city},
-    {name: 'Vila Missões', city: city},
-    {name: 'Vila N', city: city},
-    {name: 'Vila Nova', city: city},
-    {name: 'Vila Paineiras', city: city},
-    {name: 'Vila Rica', city: city},
-    {name: 'Vila S Pompeu', city: city},
+    {
+      name: 'Vila Belizario', 
+      city: city
+    },
+    {
+      name: 'Vila Carlos Humbero', 
+      city: city
+    },
+    {
+      name: 'Vila das Paineiras', 
+      city: city
+    },
+    {
+      name: 'Vila Itu', 
+      city: city
+    },
+    {
+      name: 'Vila Jardim', 
+      city: city
+    },
+    {
+      name: 'Vila Missões', 
+      city: city
+    },
+    {
+      name: 'Vila N', 
+      city: city
+    },
+    {
+      name: 'Vila Nova', 
+      city: city
+    },
+    {
+      name: 'Vila Paineiras', 
+      city: city
+    },
+    {
+      name: 'Vila Rica', 
+      city: city
+    },
+    {
+      name: 'Vila S Pompeu', 
+      city: city
+    },
     {
       name: 'Vista Alegre', 
       city: city
@@ -152,61 +315,65 @@ Neighborhood.create(
 # College
 # @implemented
 College.destroy_all
-College.create([
-  {
-    name: 'Iee Professor Isaias', 
-    type: 'Estadual',
-    city: city
-  },
-  {
-    name: 'Colégio Estadual Apolinario Porto Alegre',
-    type: 'Estadual',
-    city: city
-  },
-  {
-    name: 'Thomas Fortes',
-    type: 'Estadual',
-    city: city
-  },
-  {
-    name: 'Colégio Estadual Cristovão Pereira',
-    type: 'Estadual',
-    city: city
-  },
-  {
-    name: 'Colégio Estadual Monsenhor Assis',
-    type: 'Estadual',
-    city: city
-  }
-])
+College.create(
+  [
+    {
+      name: 'Iee Professor Isaias', 
+      type: 'Estadual',
+      city: city
+    },
+    {
+      name: 'Colégio Estadual Apolinario Porto Alegre',
+      type: 'Estadual',
+      city: city
+    },
+    {
+      name: 'Thomas Fortes',
+      type: 'Estadual',
+      city: city
+    },
+    {
+      name: 'Colégio Estadual Cristovão Pereira',
+      type: 'Estadual',
+      city: city
+    },
+    {
+      name: 'Colégio Estadual Monsenhor Assis',
+      type: 'Estadual',
+      city: city
+    }
+  ]
+)
 
 # University
 # @implemented
 University.destroy_all
-University.create([
-  {
-    name: 'UNINTER',
-    description: 'Centro universitário internacional',
-    city: city
-  },
-  {
-    name: 'UNISUL',
-    description: 'Universidade do Sul de Santa Catarina',
-    city: city
-  },
-  {
-    name: 'ULBRA',
-    description: 'Universidade Luterana do Brasil',
-    city: city
-  },
-  {
-    name: 'UNOPAR',
-    description: 'Universidade Norte do Paraná',
-    city: city
-  },
-  {
-    name: 'URI',
-    description: 'Universidade Regional Integrada do Alto Uruguai e das Missões',
-    city: city
-  }
-])
+University.create(
+  [
+    {
+      name: 'UNINTER',
+      description: 'Centro universitário internacional',
+      city: city
+    },
+    {
+      name: 'UNISUL',
+      description: 'Universidade do Sul de Santa Catarina',
+      city: city
+    },
+    {
+      name: 'ULBRA',
+      description: 'Universidade Luterana do Brasil',
+      city: city
+    },
+    {
+      name: 'UNOPAR',
+      description: 'Universidade Norte do Paraná',
+      city: city
+    },
+    {
+      name: 'URI',
+      description: 'Universidade Regional Integrada do Alto Uruguai e das Missões',
+      city: city
+    }
+  ]
+)
