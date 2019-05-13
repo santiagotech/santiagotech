9a930a58468adc241c071d4a53f35cf8e5d9cee4
Renato Rosa Franco <Renato_ny@live.com>
2019-05-13 16:12:21 -0300

    City model added

--- db/migrate/20190513191044_create_states.rb
+++ db/migrate/20190513191044_create_states.rb
@@ -1,9 +1,9 @@
 class CreateStates < ActiveRecord::Migration[5.2]
   def change
     create_table :states do |t|
-      t.string :name
-      t.string :region
-      t.string :acronym
+      t.string   :name
+      t.string   :region
+      t.string   :acronym
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_13_191044) do
+ActiveRecord::Schema.define(version: 2019_05_13_191207) do
+
+  create_table "cities", force: :cascade do |t|
+    t.string "name"
+    t.integer "state_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["state_id"], name: "index_cities_on_state_id"
+  end
 
   create_table "states", force: :cascade do |t|
     t.string "name"
