415051f87ed9e4c722b33dc2ed806fa4aabedc40
Renato Rosa Franco <Renato_ny@live.com>
2019-05-14 19:50:29 -0300

    Subscription scopes added

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -3,6 +3,15 @@ class Subscription < ApplicationRecord
   self.table_name  = 'subscriptions'
   self.primary_key = 'id'
 
+  scope :lasts,           ->{where(created_at: :asc)}
+  scope :recents,         ->{where(created_at: :desc)}
+  scope :by_state,        ->{where(state: state)}
+  scope :by_city,         ->(city){where(city: city)}
+  scope :by_neighborhood, ->(neighborhood){where( neighborhood: neighborhood)}
+  scope :by_college,      ->(college){where(college: college)}
+  scope :by_university,   ->(university){where(university: university)}
+  scope :by_profession,   ->(profession){where(profession: profession)}
+
   belongs_to :state
   belongs_to :city
   belongs_to :neighborhood
