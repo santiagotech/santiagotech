4c2c0c3787c83f7008b81cdc0538fba494cadad2
Renato Rosa Franco <Renato_ny@live.com>
2019-05-15 14:15:50 -0300

    Project updated

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -3,6 +3,8 @@ class Subscription < ApplicationRecord
   self.table_name  = 'subscriptions'
   self.primary_key = 'id'
 
+  after_create :signup_subscription_list
+
   scope :lasts,           ->{where(created_at: :asc)}
   scope :recents,         ->{where(created_at: :desc)}
   scope :by_state,        ->{where(state: state)}
@@ -18,4 +20,32 @@ class Subscription < ApplicationRecord
   belongs_to :college
   belongs_to :university
   belongs_to :profession
+
+  validates :email,
+            presence: true,
+            uniqueness: true,
+            allow_blank: false,
+            length: { minimum: 3, maximum: 245 }
+  
+  validates :rg,
+            presence: true,
+            uniqueness: true,
+            allow_blank: false,
+            length: { minimum: 3, maximum: 30}
+
+  def signup_subscription_list
+    begin
+      gibbon = Gibbon::Request.new(api_key: ENV['MAILCHIMP_API_KEY'])
+      gibbon.timeout = 30
+      gibbon.open_timeout = 30
+      gibbon.lists('c7d08a2236').members.create(body: {
+        email_address: self.email,
+        status: 'subscribed',
+        merge_fields: {
+        }
+     })
+    rescue StandardError => e
+      p 'Error'
+    end
+  end
 end
